0162f6a1ed539a92a4e31c10aac5134bb3e8872b
Khristian Tan <kistian5rctan@gmail.com>
2024-04-11 00:54:36 +0800

    very nice

---
+++ FizzBuzz.js
@@ -0,0 +1,23 @@
+// Write code below 💖
+
+for (let i = 1; i <= 15; i++) {
+    if ((i % 5 == 0) && (i % 3 == 0))
+    {
+        console.log("Fizz Buzz");
+    }
+    else if (i % 3 == 0)
+    {
+        console.log("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        console.log("Buzz");
+    }
+    else
+    {
+        console.log(i);
+    }
+}
+
+//_____________________________________________
+
